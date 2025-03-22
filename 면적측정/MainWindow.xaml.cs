941e6c34343d180434a582b1bc57b67f40882c76
lllllIIlI <93465102+lllllIIlI@users.noreply.github.com>
2025-03-22 23:34:36 +0900

    Update MainWindow.xaml.cs

--- 면적측정/MainWindow.xaml.cs
+++ 면적측정/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace KC06
             nui.SkeletonStream.Enable();
             nui.Start();
         }
+
         void nui_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
         {
             DepthImageFrame ImageParam = e.OpenDepthImageFrame();
@@ -48,50 +49,53 @@ namespace KC06
 
             image1.Source = wb;
         }
-        byte[] GetPlayer(DepthImageFrame PImage, short[] depthFrame, DepthImageStream depthstream, int nSel)
+
+        byte[] GetPlayer(DepthImageFrame PImage, short[] depthFrame, DepthImageStream depthStream)
         {
             byte[] playerCoded = new byte[PImage.Width * PImage.Height * 4];
 
             long IPixel = 0;
             long IDist = 0;
-            int nPlyer = -1;
+            int nPlayer = -1; // 플레이어 인덱스 초기값
 
-            for (int i16 = 0, i32 = 0; i16 < depthFrame.Length && i32 < playerCoded.Length;
-                 i16++, i32 += 4)
+            for (int i16 = 0, i32 = 0; i16 < depthFrame.Length && i32 < playerCoded.Length; i16++, i32 += 4)
             {
-                int player = depthFrame[i16] & DepthImageFrame.PlayerIndexBitmask;
-                int nDistance = depthFrame[i16] >> DepthImageFrame.PlayerIndexBitmaskWidth;
-                SetRGB(playerCoded, i32, 0x00, 0x00, 0x00);
+                int player = depthFrame[i16] & DepthImageFrame.PlayerIndexBitmask; // 플레이어 인덱스
+                int nDistance = depthFrame[i16] >> DepthImageFrame.PlayerIndexBitmaskWidth; // 깊이 값 계산
 
-                if (player > 0 && nPlayer <= 0) nPlayer = player;
-                if (player == nplayer)
+                SetRGB(playerCoded, i32, 0x00, 0x00, 0x00); // 기본 색 (검정)
+
+                if (player > 0 && nPlayer == -1) nPlayer = player; // 첫 번째 사람을 찾으면 nPlayer에 저장
+
+                if (player == nPlayer) // 해당 플레이어만 처리
                 {
-                    if (nDistance < depthStream.TooFarDepth &&
-                        nDiistance > depthStream.TooNearDepth)
+                    if (nDistance < 8000 && nDistance > 1000) // 거리 범위 내에서만 측정
                     {
                         IDist += nDistance;
                         IPixel += 1;
-                        SetRGB(playerCoded, i32, 0xFF, 0xFF, 0xFF)
+                        SetRGB(playerCoded, i32, 0xFF, 0xFF, 0xFF); // 흰색으로 픽셀 표시
                     }
                 }
             }
 
+            // 몸무게 추정
             if (IPixel > 0)
             {
-                textblock1.Text = string.Format("픽셀 : {0}", IPixel);
-                textblock2.Text = string.Format("거리 : {0}", IDist / IPixel);
+                textBlock1.Text = string.Format("픽셀 : {0}", IPixel);
+                textBlock2.Text = string.Format("평균 거리 : {0}", IDist / IPixel);
 
-                float weight = (IPixel * IDist) / 1000000000;
-                textblock3.Text = string.Format("무게 : {0:0} kg", weight);
+                float weight = (IPixel * IDist) / 1000000000f; // 몸무게 추정식
+                textBlock3.Text = string.Format("추정 몸무게 : {0:0} kg", weight);
             }
+
             return playerCoded;
+        }
 
-            void SetRGB(byte[] nPlayers, int nPos, byte r, byte g, byte b)
-            {
-                nPlayers[nPos + 2] = r;
-                nPlayers[nPos + 1] = g;
-                nPlayers[nPos + 0] = b;
-            }
+        void SetRGB(byte[] nPlayers, int nPos, byte r, byte g, byte b)
+        {
+            nPlayers[nPos + 2] = r;
+            nPlayers[nPos + 1] = g;
+            nPlayers[nPos + 0] = b;
         }
     }
 }
